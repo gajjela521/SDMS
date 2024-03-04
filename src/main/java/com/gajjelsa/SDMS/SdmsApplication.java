b11ce668b5a311f5ed22cde601320ab1c6e81160
GST <gajjelasuryateja2021@gmail.com>
2024-03-03 19:17:36 -0500

    Initial commit

---
+++ src/main/java/com/gajjelsa/SDMS/SdmsApplication.java
@@ -0,0 +1,13 @@
+package com.gajjelsa.SDMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gajjelsa/SDMS/SdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gajjelsa.SDMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
